3a81c5af4591b9bd1d4995f4eb0f1cfdcea7593e
Guilherme Gabriel <98980731+GuilFaria@users.noreply.github.com>
2023-06-08 23:22:14 -0300

    Ajustando atributos
    Modificação de atributo gênero.

--- SIPAv4.sql
+++ SIPAv4.sql
@@ -7,7 +7,7 @@ DROP TABLE IF EXISTS Aluno CASCADE;
 CREATE TABLE IF NOT EXISTS Aluno (
             Id_aluno SERIAL PRIMARY KEY UNIQUE NOT NULL,
             Nome_alu VARCHAR(75) not null,
-	        Sexo_alu CHAR(1) not null CHECK (Sexo_alu IN ('M', 'F')),
+	        GENERO CHAR(1) not null CHECK (GENERO IN ('M', 'F')),
 	        CPF_alu CHAR(16) unique not null,
 	        Nome_rua VARCHAR(100) not null,
 	        Num_end SMALLINT not null,
@@ -26,33 +26,33 @@ SELECT * FROM Aluno;
 ------------------
 -- Populando Aluno
 
-INSERT INTO Aluno (Nome_alu, Sexo_alu, CPF_alu, Nome_rua, Num_end, Email_alu, CEP_alu, UF, Num_cel, Data_nasc, Profissao, Bairro, Cidade)
+INSERT INTO Aluno (Nome_alu, GENERO, CPF_alu, Nome_rua, Num_end, Email_alu, CEP_alu, UF, Num_cel, Data_nasc, Profissao, Bairro, Cidade)
 VALUES ('João da Silva', 'M', '527.261.588-38', 'Rua da Juta', 63, 'joap@gmail.com', '03010-010', 'SP', 987654321, '1990-01-01', 'Comerciante', 'Brás', 'São Paulo'),
-('Pedro Gomes', 'M', '798.177.650-32', 'Avenida Graciela Flores de Piteri', 580, 'pedro@hotmail.com', '06236-215', 'SP', 987654221, '1991-12-05', NULL, 'Aliança', 'Osasco'),
-('João da Silva', 'M', '241.964.916-85', 'Praça Bacharel Fernando Braga Pereira da Rocha', 43, 'joao@gmail.com', '04417-090', 'SP', 910654321, '1990-01-01', NULL, 'Jardim Miriam', 'São Paulo'),
-('Maria Souza', 'F', '686.917.583-76', 'Estrada da Lama Preta', 321, 'maria@gmail.com', '23577-900', 'RJ', 123456789, '1995-05-15', NULL, 'Santa Cruz', 'Rio de Janeiro'),
-('Carlos Oliveira', 'M', '813.749.762-53', 'Beco da Amizade', 21, 'carlos@gmail.com', '31985-332', 'MG', 987154321, '1988-12-10', NULL, 'Dom Silvério', 'Belo Horizonte'),
-('Ana Costa', 'F', '916.257.287-35', 'Rua Tapajós', 40, 'ana@hotmail.com', '87209-074', 'PR', 654321987, '1992-07-25', NULL, 'Zona 03', 'Cianorte'),
-('Paulo Santos', 'M', '676.433.639-35', 'Rua Doutor Deoclécio Pereira', 7, 'paulo@outlook.com', '91040-470', 'RS', 321987654, '1998-03-18', NULL, 'Jardim Floresta', 'Porto Alegre'),
-('Laura Mendes', 'F', '673.668.533-80', 'Rua Brasil Toschi', 2912, 'laura@gmail.com', '18706-490', 'SP', 987654321, '1993-09-20', NULL, 'Alto da Colina II', 'Avaré'),
-('Ricardo Lima', 'M', '287.081.411-93', 'Largo Cambuci', 742, 'ricardinho@hotmail.com', '01523-010', 'SP', 123456789, '1987-06-12', NULL, 'Cambuci', 'São Paulo'),
+('Pedro Gomes', 'M', '798.177.650-32', 'Avenida Graciela Flores de Piteri', 580, 'pedro@hotmail.com', '06236-215', 'SP', 987654221, '1991-12-05', 'Diretor', 'Aliança', 'Osasco'),
+('João da Silva', 'M', '241.964.916-85', 'Praça Bacharel Fernando Braga Pereira da Rocha', 43, 'joao@gmail.com', '04417-090', 'SP', 910654321, '1990-01-01', 'Psicólogo', 'Jardim Miriam', 'São Paulo'),
+('Maria Souza', 'F', '686.917.583-76', 'Estrada da Lama Preta', 321, 'maria@gmail.com', '23577-900', 'RJ', 123456789, '1995-05-15', 'Diretora', 'Santa Cruz', 'Rio de Janeiro'),
+('Carlos Oliveira', 'M', '813.749.762-53', 'Beco da Amizade', 21, 'carlos@gmail.com', '31985-332', 'MG', 987154321, '1988-12-10', 'Pedagogo', 'Dom Silvério', 'Belo Horizonte'),
+('Ana Costa', 'F', '916.257.287-35', 'Rua Tapajós', 40, 'ana@hotmail.com', '87209-074', 'PR', 654321987, '1992-07-25', 'Psicóloga', 'Zona 03', 'Cianorte'),
+('Paulo Santos', 'M', '676.433.639-35', 'Rua Doutor Deoclécio Pereira', 7, 'paulo@outlook.com', '91040-470', 'RS', 321987654, '1998-03-18', 'Psicólogo', 'Jardim Floresta', 'Porto Alegre'),
+('Laura Mendes', 'F', '673.668.533-80', 'Rua Brasil Toschi', 2912, 'laura@gmail.com', '18706-490', 'SP', 987654321, '1993-09-20', 'Diretora', 'Alto da Colina II', 'Avaré'),
+('Ricardo Lima', 'M', '287.081.411-93', 'Largo Cambuci', 742, 'ricardinho@hotmail.com', '01523-010', 'SP', 123456789, '1987-06-12', 'Pedagogo', 'Cambuci', 'São Paulo'),
 ('Fernanda Oliveira', 'F', '691.425.633-05', 'Rua Síria', 89, 'fernanda@outlook.com', '30150-020', 'MG', 987690321, '1991-04-05', 'Professora', 'Floresta', 'Belo Horizonte'),
-('Lucas Santos', 'M', '259.719.411-60', 'Rua Rodolpho Senff Júnior', 12, 'lucas@gmail.com', '81110-130', 'PR', 654321987, '1997-02-28', NULL, 'Capão Raso', 'Curitiba'),
-('Juliana Silva', 'F', '386.218.721-70', 'Praça Província de São Pedro', 193, 'juliana@gmail.com', '91260-105', 'RS', 321987654, '1994-11-15', NULL, 'Protásio Alves', 'Porto Alegre'),
-('Carolina Fernandes', 'F', '667.762.344-00', 'Rua Felipe Camarão', 969, 'carolina@yahoo.com', '27521-330', 'RJ', 123456789, '1992-07-25', NULL, 'Liberdade', 'Resende'),
+('Lucas Santos', 'M', '259.719.411-60', 'Rua Rodolpho Senff Júnior', 12, 'lucas@gmail.com', '81110-130', 'PR', 654321987, '1997-02-28', 'Psicólogo', 'Capão Raso', 'Curitiba'),
+('Juliana Silva', 'F', '386.218.721-70', 'Praça Província de São Pedro', 193, 'juliana@gmail.com', '91260-105', 'RS', 321987654, '1994-11-15', 'Psicóloga', 'Protásio Alves', 'Porto Alegre'),
+('Carolina Fernandes', 'F', '667.762.344-00', 'Rua Felipe Camarão', 969, 'carolina@yahoo.com', '27521-330', 'RJ', 123456789, '1992-07-25', 'Psicóloga', 'Liberdade', 'Resende'),
 ('Gabriel Almeida', 'M', '835.497.616-51', 'Rua Tupaciguara', 18, 'gabriel@hotmail.com', '30330-310', 'MG', 987654021, '1996-05-03', 'Advogado', 'São Pedro', 'Belo Horizonte'),
 ('Camila Rodrigues', 'F', '862.238.218-40', 'Rua Júlio Pasa', 923, 'camila@gmail.com', '81570-510', 'PR', 654321987, '1993-03-22', NULL, 'Uberaba', 'Curitiba'),
 ('Marcelo Pereira', 'M', '164.433.466-61', 'Beco Nove', 150, 'marcelo@gmail.com', '90843-410', 'RS', 321987654, '1998-01-12', NULL, 'Nonoai', 'Porto Alegre'),
 ('Isabela Costa', 'F', '248.282.389-72', 'Rua Conde Deu', 523, 'isabela@outlook.com', '04741-901', 'SP', 987654321, '1991-09-08', 'Professora', 'Santo Amaro', 'São Paulo'),
 ('Rafaela Santos', 'F', '580.248.211-76', 'Avenida Murtas', 51, 'rafaela@gmail.com', '24913-465', 'RJ', 123456789, '1995-06-18', NULL, 'Itapeba', 'Maricá'),
 ('Gustavo Oliveira', 'M', '166.642.218-53', 'Rua Luzia Maia de Azevedo', 77, 'gustavo@gmail.com', '04877-160', 'SP', 987654321, '1990-03-30', 'Professor', 'Cidade Luz', 'São Paulo'),
-('Carla Silva', 'F', '562.321.774-52', 'Rua Coronel João Cândido Muricy', 190, 'carla@gmail.com', '82130-040', 'PR', 654321987, '1994-02-12', 'Professor', 'Abranches', 'Curitiba'),
+('Carla Silva', 'F', '562.321.774-52', 'Rua Coronel João Cândido Muricy', 190, 'carla@gmail.com', '82130-040', 'PR', 654321987, '1994-02-12', 'Professora', 'Abranches', 'Curitiba'),
 ('Vitor Rodrigues', 'M', '227.759.488-12', 'Avenida Moreira Guimarães', 4, 'vitor@gmail.com', '04074-030', 'SP', 321987654, '1999-11-25', 'Professor', 'Indianópolis', 'São Paulo'),
 ('Leticia Santos', 'F', '179.889.502-15', 'Avenida José Pinheiro Borges', 2120, 'leticia@gmail.com', '08420-092', 'SP', 987610321, '1992-08-15', NULL, 'Jardim Helena', 'São Paulo'),
 ('Rodrigo Lima', 'M', '610.578.372-88', 'Rua Clarindo Bispo de Oliveira', 84, 'rodrigo@gmail.com', '30666-110', 'MG', 987254321, '1996-05-03', 'Advogado', 'Conjunto Habitacional Vale do Jatobá (Barreiro)', 'Belo Horizonte'),
 ('Mariana Fernandes', 'F', '822.116.735-54', 'Rua Anfilófio de Carvalho', 92, 'mariana@hotmail.com', '20030-060', 'RJ', 123456789, '1993-03-22', NULL, 'Centro', 'Rio de Janeiro'),
 ('Rafael Santos', 'M', '674.087.358-52', 'Rua Ivo Stengler', 78, 'rafael@gmail.com', '85813-760', 'PR', 654321987, '1998-01-12', 'Coordenador', 'Canadá', 'Cascavel'),
-('Vanessa Pereira', 'F', '478.515.898-00', 'Rua Demóstenes', 81, 'vanea@gmail.com', '04614-013', 'SP', 321987654, '1995-09-28', NULL, 'Campo Belo', 'São Paulo');
+('Vanessa Pereira', 'F', '478.515.898-00', 'Rua Demóstenes', 81, 'vanea@gmail.com', '04614-013', 'SP', 321987654, '1995-09-28', 'Pedagoga', 'Campo Belo', 'São Paulo');
 --ARRUMAR TELEFONES (ENCONTRAR GERADOR DE NUM) E ACRESCENTAR MAIS DADOS (LEMBRANDO DE USAR OS GERADORES DE CPF E CEP)
 SELECT * from Aluno;
 --                                                                                                                                                                                                                  - ALUNO
@@ -70,8 +70,7 @@ CREATE TABLE Matricula (
 	
 	FOREIGN KEY (Id_alu) REFERENCES Aluno (Id_aluno) ON UPDATE CASCADE ON DELETE CASCADE,
 	FOREIGN KEY (Cod_curso) REFERENCES Curso (Cod_curso) ON UPDATE CASCADE ON DELETE CASCADE,
-	FOREIGN KEY (Id_monitor) REFERENCES Monitor (Id_monitor) ON UPDATE CASCADE ON DELETE CASCADE)
-;
+	FOREIGN KEY (Id_monitor) REFERENCES Monitor (Id_monitor) ON UPDATE CASCADE ON DELETE CASCADE);
 
 ---------------------
 --Populando matricula
@@ -134,7 +133,7 @@ SELECT * FROM Curso;
 
 -- OFICINA
 
-drop table if exists oficina cascade ;
+DROP TABLE IF EXISTS oficina CASCADE;
 create table oficina (
 	id_ofic serial primary key not null ,
 	nome_ofic char(20) not null ,
@@ -196,7 +195,7 @@ SELECT * from Monitor;
 -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 --ATIVIDADES
-drop table if exists Atividade cascade ;
+drop table if exists Atividade cascade;
 create table Atividade (
 	Cod_curso CHAR (5) not null,
 	num_atv integer not null primary key,
