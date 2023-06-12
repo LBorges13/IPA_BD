d0de14aface3e0d04f8cc68c088b8a9401f61b53
Lucas Borges <113699883+LBorges13@users.noreply.github.com>
2023-06-12 00:44:11 -0300

    Update SIPAv4.sql

--- SIPAv4.sql
+++ SIPAv4.sql
@@ -1,6 +1,6 @@
 SET DATESTYLE TO POSTGRES, DMY ;
 --O QUÊ FAZER
---Arrumar dados de datas, popular mais dados, acrescentar selects contendo joins.            ATUALIZADO COM O DER, FALTA AJUSTA ULTIMA TAB
+--Arrumar dados de datas, popular mais alunos, popular mais turmas, acrescentar selects contendo joins.
 
 --ALUNO
 DROP TABLE IF EXISTS Aluno CASCADE;
